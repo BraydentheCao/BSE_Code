5fa65a8d7a1cef4687cc2513fb497870d5e8737a
BraydentheCao <bcao89933@gmail.com>
2025-07-22 15:15:09 -0500

    Updated server code, it works now

--- Code/Extensions/TCPServer.py
+++ Code/Extensions/TCPServer.py
@@ -7,7 +7,7 @@ import math
 
 import socket
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+HOST = ""  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -23,43 +23,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             conn.sendall(data)
 
 
-"""
-
-app = Flask(__name__)
-
-# Open your computer's webcam (0 = default webcam)
-camera = cv2.VideoCapture(0)
-
-def generate_frames():
-    while True:
-        success, frame = camera.read()
-        if not success:
-            print("Camera not available")
-            sleep(1)  # avoid tight infinite loop
-            continue
-        else:
-            # Optional: resize or process frame here
-            _, buffer = cv2.imencode('.jpg', frame)
-            frame_bytes = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-
-@app.route('/')
-def index():
-    return '''
-    <html>
-        <body>
-            <h1>Webcam Feed</h1>
-            <img src="/video">
-        </body>
-    </html>
-    '''
-
-@app.route('/video')
-def video():
-    return Response(generate_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-"""
